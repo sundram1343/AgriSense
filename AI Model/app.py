1579022f23f20b7116001c32da5b2530e2d08602
Sundram1343 <sundramsharma2901@gmail.com>
2026-06-17 00:42:25 +0530

    completed detection part

--- AI Model/app.py
+++ AI Model/app.py
@@ -1,7 +1,35 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from tensorflow.keras.models import load_model
+from PIL import Image
+import numpy as np
 app = Flask(__name__)
+model=load_model("plant_disease_model.h5")
+with open("classes.txt","r") as f:
+    classes=[line.strip() for line in f]
 @app.route("/")
 def home():
-    return "Hello World"
+    return "Plant Disease Prediction API is running!"
+@app.route("/predict",methods=["POST"])
+def predict():
+    if 'image' not in request.files:
+        return jsonify({"error": "No image provided"}), 400
+    try:
+        file=request.files['image']
+        img=Image.open(file)
+        img=img.resize((224,224))
+        img = np.array(img) / 255.0
+        if(len(img.shape)==2):
+            img=np.stack((img,)*3,axis=-1)
+        img=np.expand_dims(img,axis=0)
+        prediction=model.predict(img)
+        confidence=float(np.max(prediction))
+        index=np.argmax(prediction)
+        disease=classes[index]
+        return jsonify({
+        "disease": disease,
+        "confidence": confidence
+        })
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 if(__name__=="__main__"):
     app.run();
--- Backend/controllers/plant-controller.js
+++ Backend/controllers/plant-controller.js
@@ -1,8 +1,35 @@
 const plant = require('../models/plant-model');
-const diseasesetection=async(req,res)=>{
+const diseasedetection=async(req,res)=>{
     try{
-        const 
+        const image=req.body.image;
+        const result=await axois.post(process.env.prediction_api_url,{image:image});
+        const plantinfo=axois.get(`https://perenual.com/api/v2/species/details/[ID]?key=${process.env.Perenual_API}`);
+        const newplant=new plant({
+            name:plantinfo.data.common_name,
+            disease:result.data.disease,
+            description:plantinfo.data.description,
+        });
+        await newplant.save();
+        if(result.disease==='')
+        {
+            res.status(200).json({message:"No disease detected"});
+        }
+        else{
+            res.status(200).json({message:"Disease detected",disease:result.data.disease});
+        }
     }catch(err){
         res.status(500).json({message:err.message})
     }
 }
+
+const wateringreminder=async(req,res)=>{
+    try{
+        const plantinfo=axois.get(`https://perenual.com/api/v2/species/details/[ID]?key=${process.env.Perenual_API}`,{image=req.body.image});
+        const plantname=plantinfo.data.common_name;
+        const {plantId, wateringTime} = req.body;
+    }catch(err){
+        res.status(500).json({message:err.message})
+    }
+}
+
+module.exports={diseasedetection, wateringreminder};
