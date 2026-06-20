a3e4f331095fac4b0f505b074bfc1a898d895295
Sundram1343 <sundramsharma2901@gmail.com>
2026-06-20 23:42:33 +0530

    working on plant controller

--- Backend/controllers/plant-controller.js
+++ Backend/controllers/plant-controller.js
@@ -11,7 +11,7 @@ const diseasedetection=async(req,res)=>{
         {
             return res.status(200).json({message:"No disease detected"});
         }
-        const plantinfo=await axios.get(`https://my-api.plantnet.org/v2/varieties?prefix=Ros&api-key=${process.env.PlantNet_API}`,{image:req.body.image});
+        const plantinfo=await axios.post(`https://my-api.plantnet.org/v2/varieties?prefix=Ros&api-key=${process.env.PlantNet_API}`,{image:req.body.image});
         const newplant=new plant({
             name:plantinfo.data.common_name,
             disease:result.data.disease,
@@ -26,17 +26,31 @@ const diseasedetection=async(req,res)=>{
 }
 const wateringreminder=async(req,res)=>{
     try{
-        let lat,lon;//lan=latitude, lon=longitude
-        get_location(lon,lat);
-        const soil_info =await axios.get(`https://www.kaegro.com/farms/api/soil?lat=${lat}&lon=${lon}`);
-        const soil_type=soil_info.data.soil_type;
         const next_watering_time =await plant.findById(id).select('nextWatering');
-        const updates_watering= water_check(next_watering_time);
-        await plant.findByIdAndUpdate(id,{nextWatering:updates_watering});
+        const updated_watering= water_check(next_watering_time);
+        await plant.findByIdAndUpdate(id,{nextWatering:updated_watering});
         await plant.save();
+        if(updated_watering-new Date()===0){
+            return res.status(200).json({message:'Watering day is today'});
+        }
     }catch(err){
         return res.status(500).json({message:err.message})
     }
 }
-
-module.exports={diseasedetection, wateringreminder};
+const set_watering_alarm=async(req,res)=>{
+    const user_input=req.body;
+    const plantinfo=await axios.post(`https://my-api.plantnet.org/v2/varieties?prefix=Ros&api-key=${process.env.PlantNet_API}`,{image:user_input.image}); 
+    let lat,lon;//lan=latitude, lon=longitude
+    get_location(lon,lat);
+    const soil_info =await axios.get(`https://www.kaegro.com/farms/api/soil?lat=${lat}&lon=${lon}`);
+    const soil_type=soil_info.data.soil_type;
+    const average_watering_time=average_watering[soil_type];
+    const last_watered=user_input.last_watered;
+    const next_watering=last_watered+average_watering_time;
+    const newplant= new plant({
+        name:plantinfo.common_name,
+        last_watered:last_watered,
+        nextWatering:next_watering
+    });
+}
+module.exports={diseasedetection, wateringreminder,set_watering_alarm};
--- Backend/utils/water_check.js
+++ Backend/utils/water_check.js
@@ -1,6 +1,11 @@
+const {get_location}= require('./Location')
+const {get_condition}=require('./weather'); 
 const water_check=(watering_time)=>{
     const current_time=new Date();
-    let lat,ln
+    let lat,lon;
+    get_location(lon,lat);
+    const weather_info=get_condition(lat,lon);
+    const condition=weather_info.weather[0].main;
     if(waterng_time-current_time===0){
         if(condition==='Rain'||condition==='Thunderstrom')
         {
