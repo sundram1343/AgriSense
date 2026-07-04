8d04cf95ad2be01dd6bee43bf8bd9fe1dbce64da
Sundram1343 <sundramsharma2901@gmail.com>
2026-07-05 01:37:07 +0530

    Working on Frontend

--- Backend/routes/plant-router.js
+++ Backend/routes/plant-router.js
@@ -1,8 +1,9 @@
 const express=require('express');
 const rourter=express.Router();
+const upload=require('../middleware/uploadmiddleware')
 const {diseasedetection,wateringreminder,set_watering_alarm,solution}=require('../controllers/plant-controller');
-rourter.post('/diseasedetection',diseasedetection);
+rourter.post('/diseasedetection',upload.single('image'),diseasedetection);
 rourter.post('/wateringreminder',wateringreminder);
-rourter.post('/set_watering_alarm',set_watering_alarm);
+rourter.post('/set_watering_alarm',upload.single('image'),set_watering_alarm);
 rourter.post('/solution',solution);
 module.exports=rourter;
